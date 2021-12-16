70674dfe3d56c8cb97bfa8d28e18d30fefc99d0c
kirill44e <85239852+kirill44e@users.noreply.github.com>
2021-12-16 11:52:28 +0300

    Add files via upload

---
+++ Другие формулы/Расчёт идельного веса.pas
@@ -0,0 +1,21 @@
+﻿Program n_30;
+ var a, r, m: real;
+ begin 
+ writeln ('Если вы - мужчина, введите 1; есди вы - женщина, введите 2');
+ readln (a);
+if a=1 then
+begin
+ writeln ('Введите свой рост в см');
+ readln (r);
+ a:=(r-100)*1.15;
+ writeln ('Ваш идеальный вес равен',' ',a,' ','кг');
+ end;
+if a=2 then
+begin
+ writeln ('Введите свой рост в см');
+ readln (r);
+ a:=(r-110)*1.15;
+ writeln ('Ваш идеальный вес равен',' ',a,' ','кг');
+ end;
+end.
+ 
---
+++ Другие формулы/Суточная норма калорий.pas
@@ -0,0 +1,29 @@
+﻿Program n_30;
+ var a, r, m, v: real;
+ begin 
+ writeln ('Если вы - мужчина, введите 1; есди вы - женщина, введите 2');
+ readln (a);
+if a=1 then
+begin
+ writeln ('Введите свой рост в см');
+ readln (r);
+ writeln ('Введите вес в кг');
+ readln (m);
+ writeln ('Введите свой возраст');
+ readln (v);
+ a:=88.36+(13.4*m)+(4.8*r)-(5.7*v);
+ writeln ('Ваша суточная норма калорий равна',' ',a,' ','ккал');
+ end;
+if a=2 then
+begin
+ writeln ('Введите свой рост в см');
+ readln (r);
+ writeln ('Введите вес в кг');
+ readln (m);
+ writeln ('Введите свой возраст');
+ readln (v);
+ a:=447.6+(9.2*m)+(3.1*r)-(4.3*v);
+ writeln ('Ваша суточная норма равна',' ',a,' ','ккал');
+ end;
+end.
+ 
