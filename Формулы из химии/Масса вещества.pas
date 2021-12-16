c19606481a5bf79d98159bc7dba60f8ec5abe577
kirill44e <85239852+kirill44e@users.noreply.github.com>
2021-12-16 11:56:04 +0300

    Add files via upload

---
+++ Формулы из химии/Масса вещества.pas
@@ -0,0 +1,10 @@
+﻿Program n_26;
+ var m, n, mr: real;
+begin
+ writeln ('Введите значение количества моль');
+ readln (n);
+ writeln ('Введите значение молярной массы (г/моль)');
+ readln (mr);
+ m:=n*mr;
+ writeln ('Масса вещества равна',' ', m);
+end.
