2826ab7f1fcef48773275546f1e03f4c7bf543b4
kirill44e <85239852+kirill44e@users.noreply.github.com>
2021-12-16 11:53:24 +0300

    Add files via upload

---
+++ Формулы из информатики/Мощность алфавита.pas
@@ -0,0 +1,13 @@
+﻿Program n_21;
+ var n, i, k: integer;
+ begin
+  writeln ('Введите значение информационного веса символа в битах');
+  readln (i);
+  n:=1;
+  k:=0;
+  repeat
+  n:=n*2;
+  k:=k+1;
+  until k=i;
+  writeln ('Мощность алфавита равна',' ', n,' ','бит');
+ end.
---
+++ Формулы из информатики/Перевод байтов, килобайтов, мегабайтов в биты.pas
@@ -0,0 +1,32 @@
+﻿Program n_25;
+ var n, a, b: real;
+ begin
+ writeln ('Чтобы перевести байты в биты, введите 1');
+ writeln ('Чтобы перевести килобайты в биты, введите 2');
+ writeln ('Чтобы перевести мегабайты в биты, введите 3');
+ readln (n);
+ if n=1 then
+ begin
+ writeln ('Введите число байтов');
+ readln (a);
+ b:=a*8;
+ writeln ('Ответ:',' ', b,' ','бит');
+ end;
+ if n=2 then
+ begin
+ writeln ('Введите число килобайтов');
+ readln (a);
+ b:=a*8*1024;
+ writeln ('Ответ:',' ', b,' ','бит');
+ end;
+ if n=3 then
+ begin
+ writeln ('Введите число мегабайтов');
+ readln (a);
+ b:=a*8*1024*1024;
+ writeln ('Ответ:',' ', b,' ','бит');
+ end;
+ end.
+ 
+ 
+ 
