01c8b59b30e348d1c2a21a82e3f9831ac01d82f9
kirill44e <85239852+kirill44e@users.noreply.github.com>
2022-03-08 21:42:30 +0300

    Delete Формулы из физики directory

--- Формулы из физики/Формула циклической частоты колебаний.pas
+++
@@ -1,9 +0,0 @@
-﻿Program n_17;
- var w, t:real;
- const p=3.14;
-begin
- writeln ('Введите значение периода колебаний');
- readln (t);
- w:=2*p/t;
- writeln ('Циклическая частота колебаний равна',' ', w);
-end.
